c94f71b23b01d796508a594202fd3246ecd95651
machariamarigi <machariamarigi@gmail.com>
2020-08-04 01:20:27 +0300

    TodoApp: Add submit functionality

--- src/Todo-App/index.tsx
+++ src/Todo-App/index.tsx
@@ -1,11 +1,20 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useState } from "react";
+
+type FormElement = React.FormEvent<HTMLElement>
 
 const TodoApp = (): JSX.Element => {
+    const [value, setValue] = useState<string>('')
+
+    const handleSubmit = (event: FormElement): void => {
+        event.preventDefault();
+        setValue('')
+    }
+
     return (
         <Fragment>
             <h1>TODO LIST</h1>
-            <form>
-                <input type="text" required />
+            <form onSubmit={handleSubmit}>
+                <input type="text" value={value} onChange={e => setValue(e.target.value)} required />
                 <button type="submit">Add Todo</button>
             </form>
         </Fragment>
