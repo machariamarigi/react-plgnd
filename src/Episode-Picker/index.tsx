6f5075aa3f86ed21dd2744a95cb714bc811fcdc6
machariamarigi <machariamarigi@gmail.com>
2020-08-06 22:03:17 +0300

    create episode picker app

---
+++ src/Episode-Picker/index.tsx
@@ -0,0 +1,12 @@
+import React, { Fragment } from 'react';
+
+const App =  (): JSX.Element => {
+    return (
+        <Fragment>
+            <h1>Rick And Morty Episode picker</h1>
+            <p>Pick your favourite episode</p>
+        </Fragment>
+    )
+}
+
+export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -2,14 +2,18 @@ import React, { Fragment } from 'react';
 import { render } from 'react-dom';
 import StateManagementKentC from './State-Management-KentC';
 import TodoApp from './Todo-App';
+import EpisodePicker from './Episode-Picker';
 
-export default function App(): React.SFCElement<{}> {
+const App = (): JSX.Element => {
     return (
         <Fragment>
             <StateManagementKentC />
             <TodoApp />
+            <EpisodePicker />
         </Fragment>
     )
 }
 
 render(<App />, document.getElementById('root'))
+
+export default App;
