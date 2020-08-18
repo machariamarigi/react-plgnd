f7e19f71ed71a848dedf7928dc2414244eff6370
machariamarigi <machariamarigi@gmail.com>
2020-08-18 17:34:25 +0300

    EpisodePicker: proper typing for StoreProvider props

--- src/Episode-Picker/Store.tsx
+++ src/Episode-Picker/Store.tsx
@@ -22,8 +22,8 @@ const reducer = (state: IState, action: IAction): IState => {
     }
 }
 
-export const StoreProvider = (props: any): JSX.Element => {
+export const StoreProvider = ({ children }: JSX.ElementChildrenAttribute): JSX.Element => {
     const [state, dispatch] = useReducer(reducer, initialState)
-    return <Store.Provider value={{ state, dispatch }}>{ props.children }</Store.Provider>
+    return <Store.Provider value={{ state, dispatch }}>{ children }</Store.Provider>
 }
 
